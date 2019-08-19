f1a477c320691670d350beeb05db55255d01408a
Jack Quail <jack@empiredigital.eu>
2019-08-19 13:41:25 +0100

    Added Guath login on Forum page

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,4 @@
 export const SET_MAP = "SET_MAP";
 export const GET_MAP = "GET_MAP";
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
+import { reducer as formReducer } from "redux-form";
 import usersReducer from "./usersReducer";
+import authReducer from "./authReducer";
 import mapReducer from "./mapReducer";
 
 export default combineReducers({
   posts: postsReducer,
   users: usersReducer,
+  auth: authReducer,
+  form: formReducer,
   map: mapReducer
 });
